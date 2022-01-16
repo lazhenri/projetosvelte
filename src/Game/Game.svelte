1957baf4ab9c251785389cad682e602f45258c30
gmatheus66 <gmatheus66@hotmail.com>
2022-01-15 21:37:01 -0300

    iniando a logica do game gerando tabelas e numero de bombas

--- src/Game/Game.svelte
+++ src/Game/Game.svelte
@@ -1,5 +1,49 @@
 <script>
+    let casas = 10;
+    let maxNBombs = 50;
+    let minNBombs = 3;
+let generate = (number) => {
+
+}
+
+
+let nBombs = ()=> {
+    return Math.floor(Math.random() * (maxNBombs - minNBombs + 1) + minNBombs)
+}
+
+let numberBombs = nBombs();
+
+let changeBlock = (cell) =>{
+    console.log("Click", cell);
+    console.log(numberBombs);
+}
 
 </script>
 
 <h2>Game</h2>
+
+<table>
+
+    {#each Array(casas) as _, i}
+        <tr>
+            {#each Array(casas) as _, nb}
+                <td on:click|preventDefault={() => changeBlock(nb)}>{nb}</td>
+            {/each}
+        </tr>
+    {/each}
+
+
+</table>
+
+
+
+<style>
+    table{
+        margin-left: 45%;
+    }
+
+    td{
+        border-style: solid;
+    }
+
+</style>
